28435bc78ed9c87b6319ed8991783ca0c45b6fb1
princ <princ@ISAAC>
2022-03-11 23:03:19 +0100

    Add project files.

---
+++ SPAWithAngular/Models/Authors.cs
@@ -0,0 +1,14 @@
+﻿namespace SPAWithAngular.Models
+{
+    public class Authors
+    {
+        public string? AuthorId { get; set; }
+        public string? Author { get; set; }
+        public int Count { get; set; }
+    }
+    public class Category
+    {
+        public string? Name { get; set; }
+        public int Count { get; set; }
+    }
+}
